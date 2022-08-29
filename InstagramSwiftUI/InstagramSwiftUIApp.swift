74c1acf017bee32b904c342dd5c78e9fc0b1714f
Camilo González <cgonzalezm1234@gmail.com>
2022-08-29 08:19:04 -0400

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Camilo Gonzalez on 29-08-22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
